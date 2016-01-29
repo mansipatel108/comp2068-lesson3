cd6b82c5abc07133046f2fd0a534bcd676bec179
Mansi <mpatel.y@gmail.com>
2016-01-29 18:11:20 -0500

    express

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 ///<referance path ="_referance.ts" /> 
-var http = require('http');
+var express = require('express');
+var app = express();
 var port = process.env.port || 3000;
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("hello node!!");
+//main route
+app.get('/', function (req, res) {
+    res.send('Hello Express!');
 });
-server.listen(port, function () {
+//Route for info page
+app.get('/info', function (req, res, next) {
+    res.send('Your custom Info page goes here');
+});
+app.listen(port, function () {
+    console.log("App started on port:" + port);
+});
+/*
+
+import http = require('http');
+
+var port:number = process.env.port || 3000;
+
+var server:http.Server = http.createServer(
+   function(req:http.ServerRequest, res:http.ServerResponse) {
+    res.writeHead(200,{'Content-Type': 'text/plain'});
+    res.end("hello node!!");
+   });
+   
+
+server.listen(port,function(){
     console.log("Server started listening on port:" + port);
 });
+
+
+*/ 
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,4 +1,66 @@
 ///<referance path ="_referance.ts" /> 
+
+import express = require('express');
+
+var app:express.Express = express();
+
+var port:number = process.env.port || 3000;
+
+//main route
+app.get('/', function (req:express.Request, res:express.Response) {
+  res.send('Hello Express!');
+});
+
+//Route for info page
+app.get('/info', function(req:express.Request, res:express.Response,next:any){
+   res.send('Your custom Info page goes here') 
+});
+
+app.listen(port, function(){
+   console.log("App started on port:" + port);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 import http = require('http');
 
 var port:number = process.env.port || 3000;
@@ -13,3 +75,6 @@ var server:http.Server = http.createServer(
 server.listen(port,function(){
     console.log("Server started listening on port:" + port);
 });
+
+
+*/
